1e5fcab49fcb1e60a9c23a5808b5345e7c20efe5
Fajar Karim <fajar.karim@btpn.com>
2020-09-11 23:25:15 +0700

    [Fajar] add header wrapper to main app

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Footer from "./components/Footer/Footer";
 import MainContent from "./components/MainContent/MainContent";
 import Restaurants from "./components/Restaurants/Restaurants";
 import './App.css';
+import HeaderWrapper from "./components/Header/HeaderWrapper";
 
 function App() {
   return (
     <div className="App">
       <Header>
-        <ActionButton/>
+        <HeaderWrapper>
+          <img src={'./asset/go-food.svg'} alt={'Logo'}/>
+          <div className={'flex align-items-center justify-space-around'} style={{width: '300px'}}>
+            <span>Dapetin App di Gocek</span>
+            <ActionButton label={"Download"}/>
+          </div>
+        </HeaderWrapper>
       </Header>
       <MainContent>
         <Restaurants/>
