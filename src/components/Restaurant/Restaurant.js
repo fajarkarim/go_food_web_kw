2a66de70158dc0167835c0526ffcf9177e50558c
Fajar Karim <fajar.karim@btpn.com>
2020-09-14 23:06:56 +0700

    [Fajar] add proptypes in restaurant component

--- src/components/Restaurant/Restaurant.js
+++ src/components/Restaurant/Restaurant.js
@@ -1,20 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Card from "../Card/Card";
 import './Restaurant.css'
 
-const Restaurant = () => {
+const Restaurant = (props) => {
+  const { restaurant: { name, thumbnail, rating, menus } } = props;
+
   return (
       <Card>
         <div className={'one-hundred-percent-height'}>
           <div className={'restaurant-card-img-wrapper'}>
-            <img src={'https://via.placeholder.com/300'} alt={'food'}/>
+            <img src={thumbnail} alt={'food'}/>
           </div>
           <div className={'restaurant-card-info-wrapper'}>
-            <h3 className={'m-0 font-small'}>Nasi Kulit Gokskin, kacangan</h3>
+            <h3 className={'m-0 font-small'}>{name}</h3>
             <div className={'flex mt-small mb-small align-items-center'}>
               <div className={'width-75'}>
                 <div>
-                  <span className={'font-small'}>Aneka nasi, Ayam & karim</span>
+                  <span className={'font-small'}>{menus}</span>
                 </div>
               </div>
               <div className={'width-25'}>
@@ -26,12 +29,28 @@ const Restaurant = () => {
                   className={'height-m'}
                   src="https://i.gojekapi.com/darkroom/gofood-indonesia/v2/images/uploads/list-rating-star-icon.png"
                   alt="rating"/>
-              <span className={'font-bold ml-small mr-small'}>4.7</span>
+              <span className={'font-bold ml-small mr-small'}>{rating}</span>
             </div>
           </div>
         </div>
       </Card>
   )
+};
+
+Restaurant.propTypes = {
+  restaurant: PropTypes.shape({
+    name: PropTypes.string,
+    thumbnail: PropTypes.string,
+    rating: PropTypes.string,
+    menus: PropTypes.string
+  })
+};
+
+Restaurant.propTypes = {
+  restaurant: 'Unnamed',
+  thumbnail: 'https://via.placeholder.com/300',
+  rating: 0.0,
+  menus: 'none'
 };
 
 export default Restaurant;
